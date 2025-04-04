5bd291da0d1d44158f71fa7a0c877932c906532a
Gertjan Goetynck <gertjan@goetynck.com>
2025-04-04 13:48:47 +0200

    Changed pipe html layout

--- src/app/default-pipe-examples/default-pipe-examples.component.ts
+++ src/app/default-pipe-examples/default-pipe-examples.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, CurrencyPipe, DatePipe, DecimalPipe, PercentPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-default-pipe-examples',
@@ -11,6 +12,11 @@ import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
 })
 export class DefaultPipeExamplesComponent {
   numberSubject: BehaviorSubject<number> = new BehaviorSubject(1);
+  currentDate = dayjs().format();
+  currencyValue = 1000.123;
+  numberValue = 999.456789;
+  percentageValue = 0.33;
+
 
   addNumberToObservable() {
     this.numberSubject.next(1 + this.numberSubject.getValue());
